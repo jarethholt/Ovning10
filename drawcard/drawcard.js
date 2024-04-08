0e9b54fd3d0c355c5bfb6981a92b89f3b28c6a37
jarethholt <jareth.holt@gmail.com>
2024-04-08 10:47:37 +0200

    Changed indentation

--- drawcard/drawcard.js
+++ drawcard/drawcard.js
@@ -1,60 +1,60 @@
 // Global variables
 const app = {
-    drawButton: document.getElementById('drawcard'),
-    mainArea: document.getElementsByTagName('main').item(0),
-    cardDiv: document.getElementById('cardDiv'),
-    drawCardAPI: new URL("https://deckofcardsapi.com/api/deck/new/draw/?count=1"),
-    cardImgWidth: 226,
-    cardImgHeight: 314,
-    cardImg: null
+  drawButton: document.getElementById('drawcard'),
+  mainArea: document.getElementsByTagName('main').item(0),
+  cardDiv: document.getElementById('cardDiv'),
+  drawCardAPI: new URL("https://deckofcardsapi.com/api/deck/new/draw/?count=1"),
+  cardImgWidth: 226,
+  cardImgHeight: 314,
+  cardImg: null
 }
 
 // First thing to do is make an appropriate amount of space for the card
 function setupCardArea() {
-    app.mainArea.style.gridTemplateRows = `${app.cardImgHeight}px`;
-    app.cardDiv.style.width = `${app.cardImgWidth}px`;
+  app.mainArea.style.gridTemplateRows = `${app.cardImgHeight}px`;
+  app.cardDiv.style.width = `${app.cardImgWidth}px`;
 }
 setupCardArea()
 
 // Set up the image element to be used
 function setupImg() {
-    if (app.cardImg !== null) return
-    app.cardImg = document.createElement('img')
-    app.cardImg.setAttribute('width', app.cardImgWidth)
-    app.cardImg.setAttribute('height', app.cardImgHeight)
+  if (app.cardImg !== null) return
+  app.cardImg = document.createElement('img')
+  app.cardImg.setAttribute('width', app.cardImgWidth)
+  app.cardImg.setAttribute('height', app.cardImgHeight)
 }
 setupImg()
 
 // Get the link to the image from the API response
 function getImageLink(response) {
-    return response.cards[0].image
+  return response.cards[0].image
 }
 
 // Set the image in the card's div
 function insertImage(imageURL) {
-    // Make sure the img tag is actually in the div
-    if (!app.cardDiv.contains(app.cardImg)) {
-        app.cardDiv.appendChild(app.cardImg)
-    }
-    app.cardDiv.classList.remove('blank')
-    app.cardImg.setAttribute('src', imageURL)
+  // Make sure the img tag is actually in the div
+  if (!app.cardDiv.contains(app.cardImg)) {
+    app.cardDiv.appendChild(app.cardImg)
+  }
+  app.cardDiv.classList.remove('blank')
+  app.cardImg.setAttribute('src', imageURL)
 }
 
 // What to run when the API call is successful
 function onSuccess(response) {
-    const imageURL = getImageLink(response)
-    insertImage(imageURL)
+  const imageURL = getImageLink(response)
+  insertImage(imageURL)
 }
 
 // What to do when the API call fails
 function onFailure(error) {
-    alert(`Error encountered with deck of cards API: ${error}`)
-    console.log(error)
+  alert(`Error encountered with deck of cards API: ${error}`)
+  console.log(error)
 }
 
 // Full function to call the draw card API
 function drawCard() {
-    fetch(app.drawCardAPI).then(res => res.json()).then(onSuccess).catch(onFailure)
+  fetch(app.drawCardAPI).then(res => res.json()).then(onSuccess).catch(onFailure)
 }
 
 // Attach the full function to the button
--- swbio/swbio.js
+++ swbio/swbio.js
@@ -1,56 +1,56 @@
 // Global variables
 const app = {
-    inputField : document.getElementById('inputField'),
-    inputButton: document.getElementById('inputButton'),
-    outputArea : document.getElementById('outputArea'),
-    baseSWURL  : new URL("https://www.swapi.tech/api/people/"),
+  inputField: document.getElementById('inputField'),
+  inputButton: document.getElementById('inputButton'),
+  outputArea: document.getElementById('outputArea'),
+  baseSWURL: new URL("https://www.swapi.tech/api/people/"),
 }
 
 // Read the search contents when the button is pressed
 function readInput() {
-    const result = app.inputField.value
-    // Convert the result into a URL with search params
-    const params = new URLSearchParams({name: result})
-    const searchURL = new URL(app.baseSWURL.toString() + '?' + params.toString())
-    return searchURL
+  const result = app.inputField.value
+  // Convert the result into a URL with search params
+  const params = new URLSearchParams({ name: result })
+  const searchURL = new URL(app.baseSWURL.toString() + '?' + params.toString())
+  return searchURL
 }
 
 // Convert a person record into a string
 function stringifyPerson(person) {
-    const props = person.properties
-    let result = `${props.name}: `
-    result += `Height ${props.height}, `
-    result += `mass ${props.mass}, `
-    result += `gender ${props.gender}, `
-    result += `hair color ${props.hair_color}`
-    return result
+  const props = person.properties
+  let result = `${props.name}: `
+  result += `Height ${props.height}, `
+  result += `mass ${props.mass}, `
+  result += `gender ${props.gender}, `
+  result += `hair color ${props.hair_color}`
+  return result
 }
 
 // Determine what to do if the API fails
 function onFailure(error) {
-    alert(`Error encountered while searching: ${error}`)
-    console.error(error)
+  alert(`Error encountered while searching: ${error}`)
+  console.error(error)
 }
 
 // Determine what to do if the API succeeds
 function onSuccess(response) {
-    const result = response.result
-    const personStrings = []
-    for (const person of result) {
-        personStrings.push(stringifyPerson(person))
-    }
-    app.outputArea.innerHTML = personStrings.join('\n')
+  const result = response.result
+  const personStrings = []
+  for (const person of result) {
+    personStrings.push(stringifyPerson(person))
+  }
+  app.outputArea.innerHTML = personStrings.join('\n')
 }
 
 // Compose the functions together
 // Should this be an async function since it uses fetch?
 function runSearch() {
-    const searchURL = readInput()
-    fetch(searchURL).then(res => res.json()).then(onSuccess).catch(onFailure)
+  const searchURL = readInput()
+  fetch(searchURL).then(res => res.json()).then(onSuccess).catch(onFailure)
 }
 
 // Attach the primary function to the search button/field
 app.inputButton.addEventListener('click', runSearch)
-app.inputField.addEventListener('keypress', function(e) {
-    if (e.key === 'Enter') runSearch()
+app.inputField.addEventListener('keypress', function (e) {
+  if (e.key === 'Enter') runSearch()
 })
